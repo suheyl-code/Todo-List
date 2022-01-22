63a7557dd78ebd79b47b41229f762b70c4e498d8
Suheyl Esadli <suheyl.asadi@gmail.com>
2022-01-22 20:14:27 +0300

    initial setup

---
+++ app.js
@@ -0,0 +1,6 @@
+// Define UI Variables
+const form = document.querySelector('#task-form');
+const taskInput = document.querySelector('#task');
+const filter = document.querySelector('#filter');
+const taskList = document.querySelector('.list-group');
+const clearBtn = document.querySelector('.clear-tasks');
