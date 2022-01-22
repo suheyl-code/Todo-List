1bfc0d7de98cc5041e848ab2fa210cb6ee626188
Suheyl Esadli <suheyl.asadi@gmail.com>
2022-01-22 21:40:22 +0300

    add-task func added

--- app.js
+++ app.js
@@ -4,3 +4,27 @@ const taskInput = document.querySelector('#task');
 const filter = document.querySelector('#filter');
 const taskList = document.querySelector('.list-group');
 const clearBtn = document.querySelector('.clear-tasks');
+
+loadEventListeners();
+
+function loadEventListeners() {
+    form.addEventListener('submit', addTask);
+}
+
+function addTask(e) {
+    if(taskInput.value === '') {
+        alert('You have to write a task first!');
+    } else {
+        const li = document.createElement('li');
+        li.className = 'list-group-item d-flex align-items-center';
+        li.appendChild(document.createTextNode(taskInput.value));
+    
+        const i = document.createElement('i');
+        i.className = 'far fa-times-circle text-danger mx-1 delete-item';
+        li.appendChild(i);
+        taskList.appendChild(li);
+
+        taskInput.value = '';
+        e.preventDefault();
+    }
+}
